183f08240801a20440027bda7ff0c20bb2fa239f
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2020-05-13 21:18:35 -0300

    Add customized status bar component.

---
+++ js/components/StatusBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { Colors } from '../assets';
+
+/**
+ * Customized status bar component for the project.
+ * @param {backgroundColor} only for android. For iOS is always transparent.
+ * @param {barStyle} for both platforms.
+ */
+
+const CustomStatusBar = ({ backgroundColor, barStyle }) => (
+  <StatusBar backgroundColor={backgroundColor} barStyle={barStyle} animated />
+);
+
+CustomStatusBar.propTypes = {
+  backgroundColor: PropTypes.oneOf(Object.values(Colors)),
+  barStyle: PropTypes.oneOf(['default', 'light-content', 'dark-content']),
+};
+
+CustomStatusBar.defaultProps = {
+  backgroundColor: Colors.WHITE,
+  barStyle: 'dark-content',
+};
+
+export default CustomStatusBar;
