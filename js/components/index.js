1c67678d826c69a4fd6f8b410897b673ea3c2f8a
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2020-05-14 23:37:08 -0300

    Add export components file.

--- js/components/index.js
+++ js/components/index.js
@@ -1,3 +1,7 @@
 import StatusBar from './StatusBar';
+import Loading from './Loading';
+import Pagination from './Pagination';
+import ArrowRight from './ArrowRight';
+import ArrowLeft from './ArrowLeft';
 
-export { StatusBar };
+export { StatusBar, Loading, Pagination, ArrowRight, ArrowLeft };
